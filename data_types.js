d492db7009a970dc8858b1eacea4fa4dda25f13a
Paras Jain <parasjain2647@gmail.com>
2026-04-03 16:04:41 +0530

    data_types

---
+++ data_types.js
@@ -0,0 +1,15 @@
+var num= 10;
+console.log(num);
+console.log(typeof(num));
+num = false;
+console.log(num);
+console.log(typeof(num));
+num = "Paras Jain";
+console.log(num);
+console.log(typeof(num));
+num = null;
+console.log(num);
+console.log(typeof(num));
+num = undefined;
+console.log(num);
+console.log(typeof(num));  
