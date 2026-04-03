a86ef6dfe94640d285c94d11d2b33f7d2f779aa0
Paras Jain <parasjain2647@gmail.com>
2026-04-03 16:35:12 +0530

    arithmetic_op2

---
+++ arithmetic_op2.js
@@ -0,0 +1,24 @@
+var a = 10;
+var b = 30;
+var linebreak = "<br>";
+
+document.write("(a+b)=>");
+result = a+b;
+document.write(result);
+document.write(linebreak);
+document.write("(a-b)=>");
+result = a-b;
+document.write(result);
+document.write(linebreak);
+document.write("(a/b)=>");
+result = a/b;
+document.write(result);
+document.write(linebreak);
+document.write("(a*b)=>");
+result = a*b;
+document.write(result);
+document.write(linebreak);
+document.write("(a%b)=>");
+result = a%b;
+document.write(result);
+document.write(linebreak);
